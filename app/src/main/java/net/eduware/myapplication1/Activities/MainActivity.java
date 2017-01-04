c73d692e513ca658a27ec5da3c2d58d1f863b566
abedbilani <abed.bilani@gmail.com>
2017-01-04 14:13:06 +0200

    2nd Commit

--- app/src/main/java/net/eduware/myapplication1/Activities/MainActivity.java
+++ app/src/main/java/net/eduware/myapplication1/Activities/MainActivity.java
@@ -1,7 +1,10 @@
 package net.eduware.myapplication1.Activities;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements
         View.OnClickListener {
 
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    static final int PICK_CONTACT_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,9 @@ public class MainActivity extends AppCompatActivity implements
                 break;
             case R.id.Map:
                 showMap();
+                break;
+            case R.id.contact:
+                pickContact();
                 break;
             default:
                 break;
@@ -82,6 +89,42 @@ public class MainActivity extends AppCompatActivity implements
 // Verify the intent will resolve to at least one activity
         if (mapIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(chooser);
+        }
+    }
+
+    private void pickContact() {
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request it is that we're responding to
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // Get the URI that points to the selected contact
+                Uri contactUri = data.getData();
+                // We only need the NUMBER column, because there will be only one row in the result
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                // Perform the query on the contact to get the NUMBER column
+                // We don't need a selection or sort order (there's only one result for the given URI)
+                // CAUTION: The query() method should be called from a separate thread to avoid blocking
+                // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+                // Consider using CursorLoader to perform the query.
+                Cursor cursor = getContentResolver()
+                        .query(contactUri, projection, null, null, null);
+                cursor.moveToFirst();
+
+                // Retrieve the phone number from the NUMBER column
+                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(column);
+                EditText setNumber = (EditText) findViewById(R.id.phone_number);
+                setNumber.setText(number);
+                // Do something with the phone number...
+            }
         }
     }
 }
